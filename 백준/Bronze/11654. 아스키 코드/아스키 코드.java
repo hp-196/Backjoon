88dfad4aedbf512fe0e99544f3186404cbd30b9c
도넛맨 <80256257+hp-196@users.noreply.github.com>
2022-07-17 23:35:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12792 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int result = s.charAt(0);
+        System.out.println(result);
+        
+       
+    
+
+    }
+    
+}
