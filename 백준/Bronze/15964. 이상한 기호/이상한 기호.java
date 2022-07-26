a7cf9b37d3cc894070fb17913190ca6ac1149a68
도넛맨 <80256257+hp-196@users.noreply.github.com>
2022-07-27 00:21:53 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] str = br.readLine().split(" ");
+    long a = Integer.parseInt(str[0]);
+    long b = Integer.parseInt(str[1]);
+    System.out.println((a+b)*(a-b));
+  }
+}
