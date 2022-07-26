90115dee7074739dc1977e638eee8eb7170e2788
도넛맨 <80256257+hp-196@users.noreply.github.com>
2022-07-26 23:23:17 +0900

    [Bronze V] Title: 문자열, Time: 148 ms, Memory: 14824 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int t = Integer.parseInt(br.readLine());
+
+      for (int i=0; i<t; i++) {
+        String str = br.readLine();
+        System.out.println(str.charAt(0) +""+ str.charAt(str.length() - 1));
+      }
+
+
+    }
+}
