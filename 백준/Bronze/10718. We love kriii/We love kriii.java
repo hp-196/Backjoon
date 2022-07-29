0210f4243109494cd1783fba0b51036eda283ccb
도넛맨 <80256257+hp-196@users.noreply.github.com>
2022-07-29 22:59:59 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("강한친구 대한육군");
+    System.out.println("강한친구 대한육군");
+  }
+}
